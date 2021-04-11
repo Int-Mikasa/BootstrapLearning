39548215b827883fa7503df28612d56d7da519ac
Mikasa <ggwplanayabeta@gmail.com>
2021-04-11 14:45:32 +0300

    added files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+  $(".content").slice(0, 8).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".content:hidden").slice(0, 4).slideDown();
+    if($(".content:hidden").length == 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+  
+})
+
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
